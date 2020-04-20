4f52d2939616fd9ef2e27ceb53c2398d020f7a29
alilu8 <32311130+alilu8@users.noreply.github.com>
2020-04-20 18:02:25 +0200

    Update controller

--- P2-Grupo 16/src/Main.java
+++ P2-Grupo 16/src/Main.java
@@ -1,14 +1,23 @@
 import controlador.Controlador;
 import modelo.Admin;
+import modelo.Estudiante;
+import modelo.Usuario;
 
 public class Main {
 
+    public static  Controlador controlador = new Controlador();
     public static void main (String[] arg){
 
-        Admin admin = new Admin();
-        Controlador controlador = new Controlador();
+        Usuario usuario = new Estudiante("Angel", "Chino", "el chino", "angel@urjc.es", "chino","estudiante");
+        Usuario usuario1 = new Estudiante("Angel", "Chino", "el chino", "angel@urjc.es", "chino","admin");
 
-        controlador.iniciarSession();
+
+        controlador.guardarUsuario(usuario);
+        controlador.guardarUsuario(usuario1);
+
+        controlador.listaUsuarios();
+
+       controlador.iniciarSession();
 
     }
 }
--- P2-Grupo 16/src/controlador/Controlador.java
+++ P2-Grupo 16/src/controlador/Controlador.java
@@ -1,6 +1,7 @@
 package controlador;
 
 import modelo.Estudiante;
+import modelo.Profesor;
 import modelo.Usuario;
 
 import java.util.ArrayList;
@@ -20,6 +21,13 @@ public class Controlador {
 
     }
 
+    public void listaUsuarios(){
+        for(int i = 0; i < usuarios.size(); i++){
+            System.out.println(usuarios.get(i).toString());
+        }
+    }
+
+
     public void borrarUsario(Usuario u){
         if(u == null){
             System.out.println("El usuario introducido no es correcto");
@@ -38,13 +46,45 @@ public class Controlador {
         String nickName = sc.nextLine();
         System.out.println("Introduce tu contraseña: ");
         String password = sc.nextLine();
-
-        for(int i = 0; i < usuarios.size(); i++){
-            if (nickName.equals(usuarios.get(i).getNick()) && password.equals(usuarios.get(i).getContraseña())){
-                System.out.println("Ha iniciado sesión correctamente");
-            }else{
-                System.out.println("usuario no se encuentra registrado");
+        try{
+            for(int i = 0; i < usuarios.size(); i++){
+                if (nickName.equals(usuarios.get(i).getNick()) && password.equals(usuarios.get(i).getContraseña())){
+                    System.out.println("Ha iniciado sesión correctamente");
+                }else{
+                    System.out.println("usuario no se encuentra registrado");
+                }
             }
+        }catch (Exception e){
+            System.out.println("Errror en el servidor "+ e.getLocalizedMessage());
+        }
+
+    }
+
+    public void registrarUsuario(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce tu nombre: ");
+        String nombre = sc.nextLine();
+        System.out.println("Introduce tus apellidos: ");
+        String apellidos = sc.nextLine();
+        System.out.println("Introduce tu nick: ");
+        String nick = sc.nextLine();
+        System.out.println("Introduce tu email: ");
+        String email = sc.nextLine();
+        System.out.println("Introduce tu contraseña: ");
+        String contraseña = sc.nextLine();
+
+
+        String[] parts = email.split("@");
+        String part1 = parts[0];
+        String part2 = parts[1];
+        if(email.equals("urjc.es")){
+            Usuario profesor = new Profesor(nombre, apellidos, nick, email, contraseña, "profesor");
+            usuarios.add(profesor);
+        }else if(email.equals("alumnos.urjc.es")){
+            Usuario estudiante = new Estudiante(nombre, apellidos, nick, email, contraseña, "estudiante");
+            usuarios.add(estudiante);
+        }else{
+            System.out.println("Los datos introducidos son erroneos. Porfavor revisalos y vuela a intentarlo");
         }
     }
 }
